42a22e5d4b1c81ec0e358459e8ac544dfb7b1262
Braden Heeney <bheeney97@gmail.com>
2025-03-30 18:06:21 -0600

    asd

---
+++ module7/milestone.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+// Module 7: concurrency and multithreading
+// CSC450-1 Braden Heeney
+//Portfolio Milestone
+
+using namespace std;
+void countUp(int x);
+void countDown(int x);
+int main(){
+    int num = 0;    
+    thread t1, t2;
+    t1 = thread(countUp, num); // Create a thread to count up from 0
+    t2 = thread(countDown, num); // Create a thread to count down from 20
+
+    t1.join();
+    t2.join(); 
+    return 0;
+}
+
+void countUp(int x){
+    while(x <= 20){
+        cout << "Count: " << x << endl; // Print current count
+        x++; // Increment count
+    }
+}
+void countDown(int x){
+    while(x >= 0){
+        cout << "Count: " << x << endl; // Print current count
+        x--; // Decrement count
+    }
+}
